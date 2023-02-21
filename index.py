cebd390d3178f01454994af0afcf8316f5c66e74
Josh <zylo.codes@gmail.com>
2023-02-21 08:56:40 -0600

    Use range() instead of dict(enumerate())

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from helpers import get_filepaths_by_extension, convert_svgs_to_pngs
 
 def main():
   try:
-    rootdir = sys.argv[1] if 1 in dict(enumerate(sys.argv)) else "./test-images"
+    rootdir = sys.argv[1] if 1 in range(len(sys.argv)) else "./test-images"
     print('Running SVG => PNG conversion in ' + rootdir)
     
     svgpaths = get_filepaths_by_extension(rootdir, 'svg')
