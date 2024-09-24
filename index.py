e7862d0af37ac73604c553f8a1536b7c09d32c08
mohit <mohit@emptyops.com>
2024-09-24 18:57:47 +0530

    commit@mohit -24-09-24

--- index.py
+++ index.py
@@ -2,19 +2,27 @@ import sys
 from helpers import get_filepaths_by_extension, convert_svgs_to_pngs
 
 def main():
-  try:
-    rootdir = sys.argv[1] if 1 in range(len(sys.argv)) else "./test-images"
-    print('Running SVG => PNG conversion in ' + rootdir)
-    
-    svgpaths = get_filepaths_by_extension(rootdir, 'svg')
-    pngpaths = convert_svgs_to_pngs(svgpaths)
+    try:
+        # Input directory: either from the command-line argument or default to "./test-images"
+        rootdir = sys.argv[1] if len(sys.argv) > 1 else "./test-images"
+        
+        # Output directory: either from the command-line argument or default to "./output-images"
+        outputdir = sys.argv[2] if len(sys.argv) > 2 else "./output-images"
+        
+        print(f'Running SVG => PNG conversion in {rootdir}, outputting to {outputdir}')
+        
+        # Fetch SVG file paths
+        svgpaths = get_filepaths_by_extension(rootdir, 'svg')
+        
+        # Convert SVGs to PNGs and save them to the output directory
+        pngpaths = convert_svgs_to_pngs(svgpaths, outputdir)
 
-    print('\nAll done. Here are your generated PNG paths:')
-    print('\n'.join(pngpaths))
+        print('\nAll done. Here are your generated PNG paths:')
+        print('\n'.join(pngpaths))
 
-    sys.exit(0)
-  except IndexError:
-    print ("Missing filepath")
-    sys.exit(1)
+        sys.exit(0)
+    except IndexError:
+        print("Missing filepath")
+        sys.exit(1)
 
 main()
